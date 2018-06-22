b720816f3461c2c092830d0ce35bde7504d2c36f
Yannick Hartmaring <yanjo@ZEDAT.FU-Berlin.DE>
2018-06-22 15:53:05 +0200

    2018.22.06.1600

--- scripts/embl2enachecklists_GUI.py
+++ scripts/embl2enachecklists_GUI.py
@@ -1,1 +1,29 @@
-import tkinter
+import Tkinter as tk
+
+
+window = tk.Tk()
+window.title("embl2enachecklist")
+window.geometry("400x400")
+
+#LABEL
+title = tk.Label(text = "embl2enachecklist")
+title.grid(column=0, row=0)
+
+#BUTTON
+button1 = tk.Button(text = "Submit")
+button1.grid()
+
+#ENTRY
+entry1 = tk.Entry()
+entry1.grid()
+
+#Dropdown
+data = ['ITS','rRNA','trnk_matK','IGS','genomic CDS']
+var = tk.StringVar()
+var.set('ITS')
+dropdown = tk.OptionMenu(window, var, *data)
+dropdown.grid()
+
+#
+
+window.mainloop()
